0ff72f8e781010221cd5607ba28ed31981988249
italocirillo <italocirillo@gmail.com>
2023-03-07 16:36:28 +0100

    ESERCIZIO COMPLETO E STILIZZATO

--- js/script.js
+++ js/script.js
@@ -2,3 +2,25 @@
 
 // Verifica collegamento javascript
 console.log("JAVASCRIPT COLLEGATO");
+
+
+//Richiesta all'utente del nome,del cognome e del colore preferto
+// Nome
+const nome= prompt("Ciao,sono un generatore di password insicuro ma funzionale, puoi dirmi il tuo nome?");
+console.log(nome);
+// Cognome
+const cognome= prompt("Bene "+nome+" adesso potremmi dirmi il tuo cognome?");
+console.log(cognome);
+// Colore
+let colore= prompt("Ok "+nome+" ultimo passaggio,puoi dirmi qual è il tuo colore preferito?");
+console.log(colore);
+// Numero password costante
+const numeroPassword=21;
+console.log(numeroPassword);
+
+
+// Salvo la password completa in una varibile e mi assicuro che sia una stringa
+let password= nome+cognome+colore+numeroPassword;
+console.log(password,"tipo variabile:",typeof password);
+// Trascrizione password sulla pagina
+document.getElementById("password-utente").innerHTML=password;
